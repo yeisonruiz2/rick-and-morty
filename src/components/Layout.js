ca6dab2b96e6defa95b5363ccea443dc467e3b04
yeisonruizruiz <jeison.3207654341@hotmail.com>
2024-11-12 20:48:23 -0500

    cambios

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,7 +14,7 @@ const Layout = () => {
                         {/* <a class="navbar-brand" href="#">Rick and Morty</a> */}
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <a class="nav-link active" aria-current="page" href="#">Informacion</a>
                             </li>
                         </ul>
                     </div>
